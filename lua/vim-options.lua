0b350587d20548c9923b9252962a157e73af24ff
Ador <adoramdev@gmail.com>
2025-01-16 12:04:53 +0330

    edit vim settings

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,7 +3,6 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
-vim.cmd("set relativenumber")
 vim.cmd("set mouse=a")
 vim.cmd("set autoindent")
 
